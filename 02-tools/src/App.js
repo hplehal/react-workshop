f827180adf6b80c90c1e4d57cf953521c3c557c4
Tej Lehal <tejlehal13@gmail.com>
2024-10-29 12:35:59 -0400

    second lesson

---
+++ 02-tools/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const Pizza = (props) => {
+    return React.createElement(
+        "div", 
+        {},[
+            React.createElement("h1", {}, props.name ? props.name : 'Hello World'),
+            React.createElement("p", {}, props.description ? props.description : 'This is an empty object description'),
+        ]
+    );
+}
+
+const App = () => {
+    return React.createElement(
+        "div", 
+        {},[
+            React.createElement("h1", {}, "Padre Gino's"),
+            React.createElement(Pizza, {
+                name: "The Hawaiian Pizza",
+                description: "Sliced Ham, Pineapple, Mozzarella Cheese"
+            }),
+            React.createElement(Pizza, {
+                name: "Americano Pizza",
+                description: "French Fries and Hot Dogs"
+            }),
+            React.createElement(Pizza, {
+                name: "The Big Meat Pizza",
+                description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+            }),
+            React.createElement(Pizza),
+        ]);
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
