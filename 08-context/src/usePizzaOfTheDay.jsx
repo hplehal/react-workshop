d5e18d85fa9780ff0f8005507fa77aa4a76af7d1
Tej Lehal <tejlehal13@gmail.com>
2024-10-31 12:58:30 -0400

    Added context

---
+++ 08-context/src/usePizzaOfTheDay.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect, useDebugValue } from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.id}` : "loading...");
+
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const res = await fetch("api/pizza-of-the-day");
+      const data = await res.json();
+      setPizzaOfTheDay(data);
+    }
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
